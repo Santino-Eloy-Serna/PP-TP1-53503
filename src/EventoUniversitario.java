c6c85a9d91d1679b839f8fb9c01a957b4c27d456
Santino-Eloy-Serna <315460378+Santino-Eloy-Serna@users.noreply.github.com>
2026-08-25 22:33:29 -0300

    Terminando EJ1

--- src/EventoUniversitario.java
+++ src/EventoUniversitario.java
@@ -4,9 +4,11 @@ public class EventoUniversitario {
     private double costoBase;
     private boolean gratuito;
     private static int cantidadEventos;
+    private static int contador;
 
     static{
         cantidadEventos = 0;
+        contador = 0;
     }
 
     public EventoUniversitario(String titulo, double costoBase, boolean gratuito) {
@@ -18,16 +20,15 @@ public class EventoUniversitario {
     }
 
     public EventoUniversitario(String Id, EventoUniversitario otro) {
-        this.Id = otro.Id;
+        this.Id = otro.Id+"-CLON";
         this.titulo = otro.titulo;
         this.costoBase = otro.costoBase;
         this.gratuito = otro.gratuito;
-        cantidadEventos++;
     }
 
     /*La funcion del segundo constructor es para "copiar" un objeto ya existente.
     * Un explicacion resumida seria: Que este nuevo contructor toma todo un objeto ya creado
-    * como el onjeto Fiesta o conferencia, y lo copia de forma excata, creando un objeto identico
+    * como el objeto Fiesta o conferencia, y lo copia de forma excata, creando un objeto identico
     * DEBO PROFUNDIZAR MAS EN ESTA "TECNICA"*/
 
     public String getId() {
@@ -67,22 +68,25 @@ public class EventoUniversitario {
     }
 
     public double CCE(){
-        System.out.print("El costo estimado es de: ");
+        System.out.print("El costo estimado del evento "+titulo+" es de: ");
         return costoBase;
     }
     // CCE = Calculo de Costo Estimado
 
     public void AS(){
-        System.out.println("Se asigna a este evento la sala: "+Id);
+        System.out.println("Se asigna al evento "+titulo+" la sala: "+Id);
     }
     //AS = Asignar Sala
+
+    public void CA(){
+        System.out.println("La actividad del evento "+titulo+" fue creada");
+        contador ++;
+    }
+    //CA = Crear Actividad
 
     public void mostrar(){
         System.out.println("El evento "+titulo+" con costo "+costoBase+" con id "+Id+" y con la cantidad de eventos de "+cantidadEventos);
         System.out.println("Actualmente el evento es gratuito? -"+gratuito);
-    }
-
-    public int getcantidadEventos() {
-        return cantidadEventos;
+        System.out.println("Actualmente el evento "+titulo+" cuenta con "+contador+" actividades");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,45 @@
 public class Main {
     public static void main(String[] args) {
 
-        EventoUniversitario fiesta = new EventoUniversitario("Festival", 10900.0, false);
+        EventoUniversitario fiesta1 = new EventoUniversitario("Fiesta", 10900.0, false);
 
-        fiesta.mostrar();
+        fiesta1.mostrar();
 
-        EventoUniversitario conferencia = new EventoUniversitario("Concierto", 0.0, true);
+        EventoUniversitario fiesta2 = new EventoUniversitario(null,fiesta1);
 
-        conferencia.mostrar();
+        fiesta2.mostrar();
+
+        EventoUniversitario conferencia1 = new EventoUniversitario("Conferencia", 0.0, true);
+
+        conferencia1.mostrar();
+
+        EventoUniversitario conferencia2 = new EventoUniversitario(null,conferencia1);
+
+        conferencia2.mostrar();
+
+        fiesta1.AS();
+        fiesta1.CA();
+        fiesta1.CCE();
+        fiesta2.AS();
+        fiesta2.CA();
+        fiesta2.CCE();
+
+        conferencia1.AS();
+        conferencia1.CA();
+        conferencia1.CCE();
+        conferencia2.AS();
+        conferencia2.CA();
+        conferencia2.CCE();
+
+        fiesta1.mostrar();
+        fiesta2.mostrar();
+        conferencia1.mostrar();
+        conferencia2.mostrar();
 
     }
 
 }
+
+// CCE = Calculo de Costo Estimado
+//AS = Asignar Sala
+//CA = Crear Actividad
